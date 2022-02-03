e4adc0978f3943143c22464f19eca0acc89b9db5
Everton Vicente <everton.vicente@onfriday.com.br>
2022-02-03 09:33:48 -0300

    Other entities and second migation

--- ProjetoMVCCusoNelio/Data/ProjetoMVCCusoNelioContext.cs
+++ ProjetoMVCCusoNelio/Data/ProjetoMVCCusoNelioContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using ProjetoMVCCusoNelio.Models;
 
 namespace ProjetoMVCCusoNelio.Data
@@ -14,6 +10,8 @@ namespace ProjetoMVCCusoNelio.Data
         {
         }
 
-        public DbSet<ProjetoMVCCusoNelio.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
