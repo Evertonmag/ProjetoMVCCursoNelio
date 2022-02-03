7b416a505faf51dbc22f605ed165ebe49029cf51
Everton Vicente <everton.vicente@onfriday.com.br>
2022-02-03 11:26:53 -0300

    Sellers Controller

---
+++ ProjetoMVCCusoNelio/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoMVCCusoNelio.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
