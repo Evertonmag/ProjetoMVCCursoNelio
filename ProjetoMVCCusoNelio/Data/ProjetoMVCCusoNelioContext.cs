3148c02a280c3964cc4aa8e0a2aad14288c7783a
Everton Vicente <everton.vicente@onfriday.com.br>
2022-02-03 08:01:02 -0300

    CRUD Scaffolding

---
+++ ProjetoMVCCusoNelio/Data/ProjetoMVCCusoNelioContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoMVCCusoNelio.Models;
+
+namespace ProjetoMVCCusoNelio.Data
+{
+    public class ProjetoMVCCusoNelioContext : DbContext
+    {
+        public ProjetoMVCCusoNelioContext (DbContextOptions<ProjetoMVCCusoNelioContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetoMVCCusoNelio.Models.Department> Department { get; set; }
+    }
+}
