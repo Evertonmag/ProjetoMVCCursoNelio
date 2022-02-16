0bd422fda2a7f18218fb7e6afc62f8e73b70c0dc
Everton Vicente <everton.vicente@onfriday.com.br>
2022-02-16 14:17:12 -0300

    App locale, number and date formatting

--- ProjetoMVCCusoNelio/Startup.cs
+++ ProjetoMVCCusoNelio/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,6 +8,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using ProjetoMVCCusoNelio.Data;
 using ProjetoMVCCusoNelio.Services;
+using System.Globalization;
+using System.Collections.Generic;
 
 namespace ProjetoMVCCusoNelio
 {
@@ -35,6 +38,16 @@ namespace ProjetoMVCCusoNelio
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SeedingService seedingService)
         {
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS },
+            };
+
+            app.UseRequestLocalization(localizationOptions);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
