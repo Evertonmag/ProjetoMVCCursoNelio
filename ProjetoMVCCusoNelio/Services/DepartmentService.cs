f0b1048ff82f7fecaa8974bb860e61ea47540db8
Everton Vicente <everton.vicente@onfriday.com.br>
2022-02-17 08:37:39 -0300

    Asynchronos operations using Task (async, await)

--- ProjetoMVCCusoNelio/Services/DepartmentService.cs
+++ ProjetoMVCCusoNelio/Services/DepartmentService.cs
@@ -1,7 +1,9 @@
-﻿using ProjetoMVCCusoNelio.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoMVCCusoNelio.Data;
 using ProjetoMVCCusoNelio.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace ProjetoMVCCusoNelio.Services
 {
@@ -14,9 +16,9 @@ namespace ProjetoMVCCusoNelio.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
 
 
